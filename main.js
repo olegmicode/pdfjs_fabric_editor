fb013147452ed458f6b4fc8fda6a99dd3583c1d8
olegmicode <olegmijovic.io@gmail.com>
2022-04-23 15:26:56 +0000

    update the package

--- main.js
+++ main.js
@@ -1,35 +1,55 @@
-// Any copyright is dedicated to the Public Domain.
-// http://creativecommons.org/licenses/publicdomain/
-
-// Hello world example for webpack.
+import 'pdfjs-dist/web/pdf_viewer.css'
+import './main.css'
 
 const pdfjsLib = require('pdfjs-dist')
+const pdfjsViewer = require('pdfjs-dist/web/pdf_viewer')
 
+if (!pdfjsLib.getDocument || !pdfjsViewer.PDFViewer) {
+    // eslint-disable-next-line no-alert
+    alert('Please build the pdfjs-dist library using\n  `gulp dist-install`')
+}
 // Setting worker path to worker bundle.
 pdfjsLib.GlobalWorkerOptions.workerSrc = './build/script/pdf.worker.bundle.js'
 
+const CMAP_URL = './build/cmaps/'
+const CMAP_PACKED = true
+
+const eventBus = new pdfjsViewer.EventBus()
+
+// (Optionally) enable hyperlinks within PDF files.
+const pdfLinkService = new pdfjsViewer.PDFLinkService({
+    eventBus
+})
+
 // Loading a document.
-function loadingPDF(pdfPath) {
-    const loadingTask = pdfjsLib.getDocument(pdfPath)
-    loadingTask.promise
-        .then(function (pdfDocument) {
-            // Request a first page
-            return pdfDocument.getPage(1).then(function (pdfPage) {
-                // Display page on the existing canvas with 100% scale.
-                const viewport = pdfPage.getViewport({ scale: 1.0 })
-                const canvas = document.getElementById('theCanvas')
-                canvas.width = viewport.width
-                canvas.height = viewport.height
-                const ctx = canvas.getContext('2d')
-                const renderTask = pdfPage.render({
-                    canvasContext: ctx,
-                    viewport
-                })
-                return renderTask.promise
-            })
-        })
-        .catch(function (reason) {
-            console.error('Error: ' + reason)
-        })
+function loadingPDF(pdfPath, container) {
+    const pdfViewer = new pdfjsViewer.PDFViewer({
+        container,
+        eventBus,
+        linkService: pdfLinkService,
+        renderer: 'svg',
+        textLayerMode: 0
+    })
+    pdfLinkService.setViewer(pdfViewer)
+
+    eventBus.on('pagesinit', function () {
+        // We can use pdfViewer now, e.g. let's change default scale.
+        pdfViewer.currentScaleValue = 'page-width'
+    })
+
+    // Loading document.
+    const loadingTask = pdfjsLib.getDocument({
+        url: pdfPath,
+        cMapUrl: CMAP_URL,
+        cMapPacked: CMAP_PACKED
+    })
+    loadingTask.promise.then(function (pdfDocument) {
+        // Document loaded, specifying document for the viewer and
+        // the (optional) linkService.
+        pdfViewer.setDocument(pdfDocument)
+        pdfLinkService.setDocument(pdfDocument, null)
+    })
 }
-loadingPDF('/docs/sample.pdf')
+document.addEventListener('DOMContentLoaded', () => {
+    loadingPDF('/docs/sample.pdf', document.getElementById('viewerContainer'))
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack') // eslint-disable-line no-unused-vars
+const CopyPlugin = require('copy-webpack-plugin')
 const path = require('path')
 
 module.exports = {
@@ -9,10 +10,31 @@ module.exports = {
     },
     mode: 'none',
     output: {
-        path: path.join(__dirname, './build/script'),
-        publicPath: './build/script/',
-        filename: '[name].bundle.js'
+        path: path.join(__dirname, './build/'),
+        publicPath: './build/',
+        filename: 'script/[name].bundle.js'
     },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(
+                        __dirname,
+                        './node_modules/pdfjs-dist/cmaps'
+                    ),
+                    to: 'cmaps'
+                }
+            ]
+        })
+    ],
     watch: true,
     watchOptions: {
         ignored: /node_modules/
